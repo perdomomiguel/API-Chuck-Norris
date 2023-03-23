e4511e2ac207eb1069f52b18710459080090db67
perdomomiguel <miguelperdomoperdomo@gmail.com>
2023-03-23 09:42:19 +0000

    Comienzo de CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const ChuckNorrisJokes = () => {
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [joke, setJoke] = useState('');
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+    fetchJoke(event.target.value);
+  }
+
+  const fetchJoke = async (category) => {
+    const url = `https://api.chucknorris.io/jokes/random?category=${category}`;
+    const response = await axios.get(url);
+    setJoke(response.data.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div>
+        <select value={selectedCategory} onChange={handleCategoryChange}>
+          <option value="">Escoge una categoría</option>
+          <option value="animal">Animal</option>
+          <option value="career">Carrera</option>
+          <option value="celebrity">Celebridad</option>
+          <option value="dev">Dev</option>
+          <option value="explicit">Explícito</option>
+          <option value="fashion">Moda</option>
+          <option value="food">Comida</option>
+          <option value="history">Historia</option>
+          <option value="money">Dinero</option>
+          <option value="movie">Películas</option>
+          <option value="music">Music</option>
+          <option value="political">Política</option>
+          <option value="religion">Religión</option>
+          <option value="science">Ciencia</option>
+          <option value="sport">Deporte</option>
+          <option value="travel">Viajes</option>
+        </select>
+        <div>{joke}</div>
+      </div>
+    </body>
   );
 }
 
-export default App;
+export default ChuckNorrisJokes;
